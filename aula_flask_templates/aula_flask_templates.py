f7f471c3926df78f967475e88ec8c3d43bbaf019
Felipe Oliveira Colen <felipecolen@gmail.com>
2016-08-18 16:22:55 -0400

    atualizando projeto de teste com templates

--- aula_flask_templates/aula_flask_templates.py
+++ aula_flask_templates/aula_flask_templates.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index')  # você pode definir várias urls para o mesmo método
 def hello_world():
+    # antes fizemos assim
+    # return 'Hello um texto qualquer com ou sem <h1>Html</h1>'
+
+    # agora só alteramos o retorno incluindo o render template
+    # por padrão, os templates tem que estar na pasta templates
     return render_template('index.html')
 
 
